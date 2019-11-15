7fed3366e858b51d3c158a59df737b40ffeb607a
Randy Zhu <30359448+randonandoz@users.noreply.github.com>
2019-11-14 18:24:48 -0800

    commit before costco & did UI
    "working" build

--- app/src/main/java/com/android/communect/MainActivity.java
+++ app/src/main/java/com/android/communect/MainActivity.java
@@ -1,8 +1,9 @@
 package com.android.communect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void makeReport(View view) {
+
+    }
+
+    public void seeReports(View view) {
     }
 }
