32a33f4d9e6b07e8319bd5e9a40f8dae1ade23a0
Randy Zhu <30359448+RandoNandoz@users.noreply.github.com>
2019-11-15 17:21:07 -0800

    commit before deleting build folder

--- app/src/main/java/com/android/communect/Uploader.java
+++ app/src/main/java/com/android/communect/Uploader.java
@@ -1,17 +1,19 @@
 package com.android.communect;
-import android.widget.ImageView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class Uploader {
-    public FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static FirebaseStorage storage = FirebaseStorage.getInstance();
+    public FirebaseFirestore db = FirebaseFirestore.getInstance();
     public StorageReference storageReference = storage.getReference();
 
     public static boolean Upload(String FilePath) {
+        // TODO Make this work lol
         StorageReference storageReference = storage.getReference();
         StorageReference pictureReference = storageReference.child(FilePath);
+        return false;
+
     }
 }
